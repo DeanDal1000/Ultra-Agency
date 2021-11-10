95577341f2e6fffbf379f12195abfc7cd3b99bf7
DeanDal1000 <dalzelldean@gmail.com>
2021-11-10 16:58:05 +0000

    First Commit

---
+++ components/Navbar.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import styles from '../styles/Navbar.module.css';
+import Link from 'next/link';
+
+const Navbar = () => {
+  return (
+    <div className={styles.container}>
+      <Link href="/">Ultra</Link>
+      <ul className={styles.list}>
+        <Link href="/products/design">
+          <li className={styles.list_item}>Design</li>
+        </Link>
+        <Link href="/products/development">
+          <li className={styles.list_item}>Development</li>
+        </Link>
+        <Link href="/products/production">
+          <li className={styles.list_item}>Production</li>
+        </Link>
+        <Link href="/products/photography">
+          <li className={styles.list_item}>Photography</li>
+        </Link>
+        <Link href="/products/contact">
+          <li className={styles.list_item}>Contact</li>
+        </Link>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
